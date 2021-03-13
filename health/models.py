dc067296bf984f0d6b7b37c3d44b7ad6251429b0
Demarillac Izere <izered3@gmail.com>
2021-03-13 13:39:48 +0200

    added new pharmacies

--- health/models.py
+++ health/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.db.models.signals import post_save
 import datetime as dt
-from django.contrib.gis.db import models  
+# from django.contrib.gis.db import models  
 class Location(models.Model):
     name=models.CharField(max_length=50)
 
